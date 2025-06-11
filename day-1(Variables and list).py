8442e5529a47b0faf28854212e70a0802884978d
Sandeep Nelluru <sandeepnelluru@gmail.com>
2025-06-11 09:38:59 +0530

    Add files via upload

---
+++ day-1(Variables and list).py
@@ -0,0 +1,30 @@
+#how to print anything in python
+#print("hello world")
+#how to print name taking user input
+#name = input("Enter your name: ")
+#print(name)
+#Variables
+# price = 10
+# rating = 4.9
+# name = "sandeep"
+# print(price)
+#we check in a patient named john smith. he's 20 years old and is a new patient
+# name = "John Smith"
+# age = 20
+# patient = "New"
+# print("name :", name)
+# print("age :", age)
+# print("new/old patient :",patient)
+# taking user input enter name and favourite color and print message like "jon likes blue"
+# name = input("Enter your name: ")
+# color = input("Enter your favourite :")
+# print(f"{name} likes {color}")
+#find your age
+# year = int(input("Enter your birth year: "))
+# age = 2025 - year
+# print(age)
+# print(type(age))
+#ask a user their weight in pounds, convert it to kilograms and print on the terminal
+weight = float(input("Enter your weight in pounds: "))
+kg = (weight*0.45)
+print(kg)
